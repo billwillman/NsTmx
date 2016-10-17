6a1105308cca1b2de40382298083d52ab22daaeb
billwillman <billwillman@qq.com>
2016-10-17 19:52:05 +0800

    【增加】MESHNDOE

--- NsTmx/Assets/Script/NsTmx/TMXMeshManager.cs
+++ NsTmx/Assets/Script/NsTmx/TMXMeshManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using Utils;
 
+// AOI算法
 // TMX可视范围的MESH管理
 public class TMXMeshManager
 {
@@ -15,7 +16,37 @@ public class TMXMeshManager
 
 	}
 
+	public TMXMeshNode XFirstNode
+	{
+		get {
+			return m_XFirstNode;
+		}
+	}
+
+	public TMXMeshNode YFirstNode
+	{
+		get {
+			return m_YFirstNode;
+		}
+	}
+
 	private void Clear()
 	{
+		TMXMeshNode node = m_XFirstNode;
+
+		while (node != null) {
+
+
+
+			node = node.XNodeNext;
+		}
+
+		m_XFirstNode = null;
+		m_YFirstNode = null;
 	}
+
+	// 池
+	private ObjectPool<TMXMeshNode> m_Pool = null;
+	private TMXMeshNode m_XFirstNode = null;
+	private TMXMeshNode m_YFirstNode = null;
 }
---
+++ NsTmx/Assets/Script/NsTmx/TMXMeshNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class TMXMeshNode: MonoBehaviour
+{
+	/* 节点 */
+
+	public TMXMeshNode XNodePrev {
+		get;
+		set;
+	}
+
+	public TMXMeshNode XNodeNext {
+		get;
+		set;
+	}
+
+	public TMXMeshNode YNodePrev
+	{
+		get;
+		set;
+	}
+
+	public TMXMeshNode YNodeNext
+	{
+		get;
+		set;
+	}
+
+	//---------------------------------------------
+
+	public Mesh mesh
+	{
+		get
+		{
+			return m_Mesh;
+		}
+	}
+
+	public void Destroy()
+	{
+	}
+
+	void OnDestroy()
+	{
+		Destroy ();
+	}
+
+	private Mesh m_Mesh = null;
+}
+
