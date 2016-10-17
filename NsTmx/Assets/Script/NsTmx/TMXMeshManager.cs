2779a93c5ec8fc6681aa7aafa69ebb9d38cb0cf7
billwillman <billwillman@qq.com>
2016-10-17 19:22:32 +0800

    【修改】TMXMESH管理部分代码

---
+++ NsTmx/Assets/Script/NsTmx/TMXMeshManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+
+// TMX可视范围的MESH管理
+public class TMXMeshManager
+{
+	public void Build(Camera cam)
+	{
+		Clear ();
+
+		if (cam == null)
+			return;
+
+
+	}
+
+	private void Clear()
+	{
+	}
+}
